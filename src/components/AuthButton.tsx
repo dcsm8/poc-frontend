80080951ae4d77405e044481a13c56f8ed29cfb1
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2024-08-21 15:25:45 -0500

    first commit

---
+++ src/components/AuthButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function AuthButton() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+}
